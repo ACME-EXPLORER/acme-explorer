a31aa56a1249794fbebb14172b116c6bbfd88ac6
David Bujosa <davidbujosa@gmail.com>
2022-02-14 05:32:07 +0100

    feat(application): add timestamps

--- backend/src/modules/application/schemas.js
+++ backend/src/modules/application/schemas.js
@@ -1,14 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 export default {
-  Application: new Schema({
-    explorer: { type: Schema.Types.ObjectId, ref: 'Actor' },
-    state: { type: String, required: true, enum: ['pending', 'rejected', 'due', 'accepted'], default: 'pending' },
-    comment: String,
-    rejectedReason: String,
-    isPaid: { type: Boolean, default: false },
-    createdAt: Number,
-    updatedAt: Number
-  })
+  Application: new Schema(
+    {
+      explorer: { type: Schema.Types.ObjectId, ref: "Actor" },
+      state: {
+        type: String,
+        required: true,
+        enum: ["pending", "rejected", "due", "accepted"],
+        default: "pending",
+      },
+      comment: String,
+      rejectedReason: String,
+      isPaid: { type: Boolean, default: false },
+    },
+    { timestamps: true }
+  ),
 };
