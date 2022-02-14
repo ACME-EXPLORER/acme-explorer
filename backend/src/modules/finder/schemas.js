c80ce38ddf5f16122d3a59b7d972be891beb740e
David Bujosa <davidbujosa@gmail.com>
2022-02-14 05:34:45 +0100

    fix(application): add createdAt and updatedAT

--- backend/src/modules/finder/schemas.js
+++ backend/src/modules/finder/schemas.js
@@ -1,8 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 export default {
-  Finder: new Schema({
-    name: String
-  })
+  Finder: new Schema(
+    {
+      name: String,
+    },
+    { timestamps: true }
+  ),
 };
