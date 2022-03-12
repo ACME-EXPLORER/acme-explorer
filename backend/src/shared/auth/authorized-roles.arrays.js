07795c56f5c7bd4ca90823b420873c1601509030
David Bujosa <davidbujosa@gmail.com>
2022-03-12 11:04:51 +0100

    feat: add authorized roles

---
+++ backend/src/shared/auth/authorized-roles.arrays.js
@@ -0,0 +1,18 @@
+import Roles from '../enums.js';
+
+export const EXPLORER = [
+    Roles.ADMIN,
+    Roles.EXPLORER
+];
+
+export const MANAGER = [
+    Roles.ADMIN,
+    Roles.EXPLORER
+];
+
+export const SPONSOR = [
+    Roles.ADMIN,
+    Roles.SPONSOR
+];
+
+
