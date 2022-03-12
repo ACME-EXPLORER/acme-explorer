ae08e2044d98f0f740ebd942dd91ded9386a40bd
David Bujosa <davidbujosa@gmail.com>
2022-03-12 10:41:07 +0100

    feat: add login routes

---
+++ backend/src/routes/index.js
@@ -0,0 +1,7 @@
+export * from './actorRoutes.js';
+export * from './applicationRoutes.js';
+export * from './finderRoutes.js';
+export * from './sponsorshipRoutes.js';
+export * from './tripRoutes.js';
+export * from './loginRoutes.js';
+export * from './dashboardRoutes.js';
